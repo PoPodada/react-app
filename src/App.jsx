d7982ada595fc5265189bb87d90a46370dbd19d0
go <goito.722@icloud.com>
2024-01-30 19:04:32 +0900

    react practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react";
+import  ColorfulMessage from "./components/Colorfullmessage";
+
 export const App = ()=>{
+  console.log("--app--")
+  const [num,setNum] = useState(0);
+  const [isShowFace,setIsShowFace] = useState(true);
+  const onClickCountUp = () =>{
+    setNum((prev)=>prev +1);
+    setNum (num +1);
+  }
+  const onClickToggle = () =>{
+    setIsShowFace(!isShowFace);
+  }
+  useEffect(()=>{
+    if(num >0){
+      if(num % 3 === 0) {
+        isShowFace || setIsShowFace(true);
+      }else{
+        isShowFace && setIsShowFace(false)
+      }
+    }
+    
+  },[]);
+  // num
+  
+
   return(
     <>
-    <h1>konnnitiha</h1>
-    <button onClick={()  => alert()}>ボタン</button>
+    <h1 style={{ color:"blue"}}>konnnitiha</h1>
+    <ColorfulMessage color = "blue">kljalfkasjfa</ColorfulMessage>
+    <ColorfulMessage color = "red">aiueo</ColorfulMessage>
+    
+    <button onClick={onClickCountUp}>カウントアップ</button>
+    <p>{num}</p>
+    <button onClick={onClickToggle}>on/off</button>
+
+    {isShowFace && <p>何を四天王</p>}
     </>
   );
 };
---
+++ src/components/Colorfullmessage.jsx
@@ -0,0 +1,12 @@
+const ColorfulMessage = (props) =>{
+    const {color,children} = props;
+    const contentStyleA ={
+        color,
+        fontSize:"18px"
+    };
+    return(
+        <p style={contentStyleA}>{children}</p>
+    )
+}
+
+export default ColorfulMessage;
